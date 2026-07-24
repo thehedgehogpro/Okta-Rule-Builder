4088c75a534f0149f467722f396587dd84d618ea
HedgehogPro <thehedgehogpro@gmail.com>
2026-07-24 12:23:29 -0500

    Delete background.js

--- background.js
+++
@@ -1,4 +0,0 @@
-// Open the builder as a full page in its own tab when the toolbar icon is clicked.
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
-});
